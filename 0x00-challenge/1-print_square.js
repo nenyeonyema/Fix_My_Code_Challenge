59581d3e627e6fd59979861927eb97c99eca345d
nenyeonyema <chinenyeonyema1@gmail.com>
2023-12-23 09:23:47 -0800

    print square

--- 0x00-challenge/1-print_square.js
+++ 0x00-challenge/1-print_square.js
@@ -6,11 +6,12 @@
     of the program.
 */
 
-if (process.argv.length <= 2) {
-    process.stderr.write("Missing argument\n");
-    process.stderr.write("Usage: ./1-print_square.js <size>\n");
-    process.stderr.write("Example: ./1-print_square.js 8\n");
-    process.exit(1);
+if (process.argv.length <= 2)
+{
+	process.stderr.write("Missing argument\n");
+	process.stderr.write("Usage: ./1-print_square.js <size>\n");
+	process.stderr.write("Example: ./1-print_square.js 8\n");
+	process.exit(1);
 }
 
 let size = parseInt(process.argv[2]);
